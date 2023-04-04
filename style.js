83b7e7eb0b9473281803920a4f335de97a449ffd
haze1109 <hazemorales11@gmail.com>
2023-04-04 12:45:34 +0800

    6

---
+++ style.js
@@ -0,0 +1,13 @@
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+
+let counter = 1;
+const size = slides[0].clientWidth;
+
+slider.style.transform = `translateX(${-size * counter}px)`;
+
+setInterval(() => {
+  if (counter >= slides.length - 1) return;
+  counter++;
+  slider.style.transform = `translateX(${-size * counter}px)`;
+}, 3000);
